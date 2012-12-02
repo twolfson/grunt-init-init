ac2b5c2449367d33e985a073bb5c1cb5c0bebcce
Todd Wolfson <todd@twolfson.com>
2012-12-01 20:06:13 -0800

    Exposing the standard templates

---
+++ scratch/getStandardTemplates.js
@@ -0,0 +1,10 @@
+// Load up grunt and fetch the task dir path
+var path = require('path'),
+    grunt = require('grunt'),
+    gruntDir = require.resolve('grunt'),
+    initDir = path.join(gruntDir, '../../tasks/init'),
+    templates = grunt.file.expandFiles(initDir + '/*.js');
+
+console.log(templates);
+// Export the the standard templates
+module.exports = templates;
