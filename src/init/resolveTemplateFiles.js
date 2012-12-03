047a46acbd74a092dd83b79f54e0493b65bb917e
Todd Wolfson <todd@twolfson.com>
2012-12-02 21:41:54 -0800

    Resolving files for custom and standard templates

--- src/init/resolveTemplateFiles.js
+++ src/init/resolveTemplateFiles.js
@@ -1,4 +1,27 @@
+// Load in grunt
+var grunt = require('grunt'),
+    paths = require('./paths'),
+    customDir = paths.custom,
+    stdDir = paths.standard,
+    expandFiles = grunt.file.expandFiles;
+
+// Define a helper to find custom and standard template files
 function resolveTemplateFiles(name) {
+  // Create paths for resolving
+  var customFile = customDir + '/' + name + '.js',
+      customTemplateDir = customDir + '/' + name + '/**/*',
+      stdFile = stdDir + '/' + name + '.js',
+      stdTemplateDir = stdDir + '/' + name + '/**/*';
+
+  // Grab any and all files
+  var customFiles = expandFiles(customFile).concat(expandFiles(customTemplateDir)),
+      stdFiles = expandFiles(stdFile).concat(expandFiles(stdTemplateDir));
+
+  console.log(customFiles, stdFiles);
+
+  // TODO: Use filter to remove custom + standard cross-over
 
 }
+
+// Expose resolveTemplateFiles
 module.exports = resolveTemplateFiles;
