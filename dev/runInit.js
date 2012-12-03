25cb177f4f0a147dd07e39fb4e6c8878c2bc4dc4
Todd Wolfson <todd@twolfson.com>
2012-12-03 02:28:12 -0800

    Finally writing out files

--- dev/runInit.js
+++ dev/runInit.js
@@ -1,9 +1,14 @@
 function noop() {}
 var pathPrefix = 'src/init/';
 
+// Change the working directory
+process.chdir(__dirname + '/../../init-dev');
+
 var grunt = require('grunt'),
+    path = require('path'),
     init = {
       'renames': {},
+      // https://github.com/gruntjs/grunt/blob/master/tasks/init.js#L91-L107
       'filesToCopy': function(props) {
         var files = {};
         // Iterate over all source files.
@@ -19,6 +24,37 @@ var grunt = require('grunt'),
         return files;
       },
       'addLicenseFiles': noop,
+      // https://github.com/gruntjs/grunt/blob/master/tasks/init.js#L108-L175
+      'srcpath': function(arg1) {
+        if (arg1 == null) { return null; }
+        var args = ['init', name, 'root'].concat(grunt.utils.toArray(arguments));
+        return grunt.task.getFile.apply(grunt.file, args);
+      },
+      'destpath': path.join.bind(path, process.cwd()),
+      'copy': function(srcpath, destpath, options) {
+        // Destpath is optional.
+        if (typeof destpath !== 'string') {
+          options = destpath;
+          destpath = srcpath;
+        }
+        // Ensure srcpath is absolute.
+        if (!grunt.file.isPathAbsolute(srcpath)) {
+          srcpath = init.srcpath(srcpath);
+        }
+        // Use placeholder file if no src exists.
+        if (!srcpath) {
+          srcpath = grunt.task.getFile('init/misc/placeholder');
+        }
+        grunt.verbose.or.write('Writing ' + destpath + '...');
+        try {
+          grunt.file.copy(srcpath, init.destpath(destpath), options);
+          grunt.verbose.or.ok();
+        } catch(e) {
+          grunt.verbose.or.error().error(e);
+          throw e;
+        }
+      },
+      // https://github.com/gruntjs/grunt/blob/master/tasks/init.js#L91-L107
       'copyAndProcess': function(files, props, options) {
         console.log(files);
         options = grunt.utils._.defaults(options || {}, {
@@ -47,18 +83,12 @@ var grunt = require('grunt'),
     },
     initTemplate = require('../src/init.js');
 
-// Hackish override for grunt.task.expandFiles
+// Add in our searchDir
 // https://github.com/gruntjs/grunt/blob/master/lib/grunt/task.js#L209-L255
-// grunt.task.expandFiles = grunt.file.expandFiles;
 grunt.task.searchDirs.push(__dirname + '/..');
-// console.log(grunt.task);
-console.log(   pathPrefix, grunt.task.expandFiles({dot: true}, pathPrefix + '**'))
 
 // Load the default grunt tasks
 grunt.task.loadTasks(__dirname + '/../node_modules/grunt/tasks');
-
-// Change the working directory
-process.chdir(__dirname + '/../../init-dev');
 
 initTemplate.template(grunt, init,  function() {
   // Fail task if errors were logged.
