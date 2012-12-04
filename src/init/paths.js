2435c6e3af147637c43dfbccddf5c74816fb796a
Todd Wolfson <todd@twolfson.com>
2012-12-04 01:10:56 -0800

    Resolving grunt via require cache due to ~/.grunt complications

--- src/init/paths.js
+++ src/init/paths.js
@@ -2,9 +2,33 @@
 var grunt = require('grunt'),
     path = require('path');
 
+// Grab the keys of require.cache
+var requireCache = require.cache,
+    cacheKeys = Object.getOwnPropertyNames(requireCache);
+
+// Find the first key that ends with grunt.js
+var i = 0,
+    len = cacheKeys.length,
+    cacheKey,
+    gruntPath = '';
+for (; i < len; i++) {
+  cacheKey = cacheKeys[i];
+
+  // If the cacheKey ends with 'grunt.js', save it and stop searching
+  if (cacheKey.slice(-8) === 'grunt.js') {
+    gruntPath = cacheKey;
+    break;
+  }
+}
+
+// If there is no gruntPath, throw an error
+if (!gruntPath) {
+  throw new Error('Path to grunt could not be found! =(');
+}
+
 // Standard path
-var gruntPath = require.resolve('grunt'),
-    stdDir = path.join(gruntPath, '../../tasks/init');
+// var gruntPath = require.resolve('grunt'),
+var stdDir = path.join(gruntPath, '../../tasks/init');
 
 // Custom path (this will be null if it does not exist)
 var customDir = grunt.file.userDir('tasks/init');
